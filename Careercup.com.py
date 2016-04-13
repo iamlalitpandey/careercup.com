9b54d800d80bec5b0af65ea93067a17cd72970d3
iamlalitpandey <lalitatg@gmail.com>
2016-04-12 22:32:32 -0400

    initial Scraper, for every questions,time & tags.

--- Careercup.com.py
+++ Careercup.com.py
@@ -4,54 +4,53 @@ Created on Thu Mar 10 19:19:40 2016
 
 @author: Lalit
 """
-import re
-import time#,sys
+import re,sys,time
 from selenium import webdriver
 
-driver = webdriver.Chrome('chromedriver.exe')
+#reload(sys)  
+
+# Set default python encoding to utf-8 instead of asciii.
+sys.setdefaultencoding('utf8')
+#sys.getdefaultencoding()
+
+driver = webdriver.Chrome('C:\\Users\\lalit\\Desktop\\Stevens\\Python\\careerCup\\chromedriver.exe')
 
 def parsePage(html):
-    p = re.compile('<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\<\>]*?)</p>.*?<a href="/user.*popup\(.*\)\;\"\>(.*?)</a>')    
-    Ms=re.finditer(p,html)
-    for M in Ms:
-        fw.write(M.group(1)+'\n')
-        fw.write(M.group(2)+'\n')    
+#p = re.compile('<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?)</p>.*[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?(<span class="tags">.*[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?</span>)')
+    p=re.compile('<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?)</p>.*[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?<abbr.*?Z">(.*?)</abbr>.*[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?(<span class="tags">.*[\sa-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?</span>)')
     
-    #questions=re.findall('<span class="entry">(.*?)<a href="/(question=.*?)><p>(.*?)</p>(.*?)</a>(.*?)</span>',html)    
-    #for question in questions:
-     #   print question.group(1).strip()        
-        #userSet.add(user.group(1).strip())
-fw=open('questions.txt','a+')
+    #working 1 #  '<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?)</p>.*?[\s\-\<a-zA-Z\/\>\=\"\:\.\?\d\(\)\;]*?<abbr.*class="timeago".*?">(.*?)</abbr>.*[a-zA-Z\s\<\_\=\"\d\/\?\>\|\:\.]*?\
+            #[\s\<\>\:\/\\\?a-zA-Z\_\=\"\d\|\.]*?(<span.*class="tags">.*[\s\<\>\:\/\\\?a-zA-Z\_\=\"\d\|\.\-]*?</span>)')    
+    # Working 2 #for 3 tags '<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\</>]*?)</p>.*?[\s\-\<a-zA-Z\/\>\=\"\:\.\?\d\(\)\;]*?<abbr.*class="timeago".*?\"\>(.*?)\<\/abbr>[a-zA-Z\s\<\_\=\"\d\/\?\>\|\:\.]*?(<span.*class="tags">.*[a-zA-Z\s\<\=\"\:\/\/\?\.\-\>]*?</span>)')
+    #'<a.*?href.*?question.*?<p>([a-zA-Z\s\,\[\]\(\)\.\'\"\#\?\@\`\%\^\*\:\=\d\&\;\_\{\}\!\$\+\\\/\-\|\…\<\>]*?)</p>.*?<a href="/user.*popup\(.*\)\;\"\>(.*?)</a>')    
+    Ms=re.finditer(p,html)
+    for count,M in enumerate(Ms):
+        fw.write('\n'+"----------- Question :"+str(count+1)+"-----------"+'\n')        
+        fw.write(M.group(1)+'\n')
+        fw.write('\n'+M.group(2)+'\n')
+        tagshtml=M.group(3)
+        tags=re.finditer('<a.*?[a-z\<\s\=\"\>\/\?\-]*?>(.*?)</a>',tagshtml)
+        for tag in tags:
+            fw.write('\n'+tag.group(1))
+        
+fw=open('dataset43.txt','a+')
 page=1
 while True:
     
- #   try:    
+    try:    
         url='https://www.careercup.com/page?n='+str(page)
-        #fconn=open("C:\\Users\\lalit\\Desktop\\Stevens\\Python\\careerCuP\\1.html")
-        #htmldat=fconn.read()#read the entire html into this variable
-        #fconn.close()
-        #type(htmldat)
-        #print htmldat
         driver.get(url)
         htmldat=driver.page_source
-        #htmldat1=htmldat.replace("<br/>","")
-        html=htmldat.replace("<br />","")
-        #html=htmldat.replace("<br />,"")
+        
+        html1=htmldat.replace("<br />","")
+        html=html1.replace("<br>","")
+        print html
         parsePage(html)
-        
-#[] and <>
-
-#<a.*?href.*?question.*?<p>([a-zA-Z\_\:\d\&\;\=\.\*\s\S\n].*[\s].*?[\.\s].*)?</p>')
-
-        
-      
-        #parsePage(driver.page_source)
-        #button=driver.find_element_by_css_selector(cssPath)
-   # except:
-       # error_type, error_obj, error_info = sys.exc_info()
-     #   print 'STOPPING - COULD NOT FIND THE LINK TO PAGE: ', page
-     #   print error_type, 'Line:', error_info.tb_lineno
-     #   break
+    except:
+        error_type, error_obj, error_info = sys.exc_info()
+        print 'STOPPING - COULD NOT FIND THE LINK TO PAGE: ', page
+        print error_type, 'Line:', error_info.tb_lineno
+        break
         time.sleep(2)
         page+=1
 fw.close()
